cf5644ef18f73321aa8561cae22e80a171487cdc
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-11-05 16:26:45 -0600

    link internal pages and add show info

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import Container from './container'
 import Hero from './hero'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <Container>
-    </Container>
+    
+      <Container>
+      </Container> 
     
   )
 }
--- src/home_nav.jsx
+++ src/home_nav.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
+import Shows from './shows'
+import './App.css'
+
+function ShowLink() {
+    return <li className="nav-item"><a href="" className="nav-link">Shows</a></li>
+}
+
+function MusicLink() {
+    return <li className="nav-item"><a href="" className="nav-link">Music</a></li>
+}
 
 function HomeNav() {
   return (
-    <nav className="home-nav">
-      <ul className="nav-list">
-        <li className="nav-item"><a href="" className="nav-link">Shows</a></li>
-        <li className="nav-item"><a href="" className="nav-link">Music</a></li>
-      </ul>
-    </nav>
+    <BrowserRouter>
+      <nav className="home-nav">
+        <ul className="nav-list">
+            <Link to="/shows">Shows</Link>
+            <Link to="/music">Music</Link>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/shows" element={<Shows />} />
+        <Route path="/music" element={<MusicLink />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
