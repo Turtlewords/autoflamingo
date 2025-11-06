5a4f660ba2a055c5f3a3b7e3ecdbb28813da4b54
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-11-05 20:01:47 -0600

    work on shows page

--- src/home_nav.jsx
+++ src/home_nav.jsx
@@ -1,29 +1,23 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 import Shows from './shows'
+import Music from './music'
 import './App.css'
 
-function ShowLink() {
-    return <li className="nav-item"><a href="" className="nav-link">Shows</a></li>
-}
-
-function MusicLink() {
-    return <li className="nav-item"><a href="" className="nav-link">Music</a></li>
-}
 
 function HomeNav() {
   return (
     <BrowserRouter>
       <nav className="home-nav">
         <ul className="nav-list">
-            <Link to="/shows">Shows</Link>
-            <Link to="/music">Music</Link>
+            <Link className="nav-link" to="/shows">Shows</Link>
+            <Link className="nav-link" to="/music">Music</Link>
         </ul>
       </nav>
 
       <Routes>
         <Route path="/shows" element={<Shows />} />
-        <Route path="/music" element={<MusicLink />} />
+        <Route path="/music" element={<Music />} />
       </Routes>
 
     </BrowserRouter>
---
+++ src/music.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Music() {
+    return (
+        <div className="music-container">
+            <h2>Under Construction</h2>
+        </div>
+    )
+}
+
+export default Music;
