98d04fe274f4a05b3fc8a2c0ba74a6d6f4e4abbb
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-11-08 16:51:52 -0600

    adjust img filepath

--- src/home.jsx
+++ src/home.jsx
@@ -2,7 +2,7 @@
 function Home() {
     return (
         <div className="home fade-in">
-            <img className="hero-img" src="src/assets/af_burlington_lg.jpg" alt="autoflamingo on stage"/>
+            <img className="hero-img" src="./src/assets/af_burlington_lg.jpg" alt="autoflamingo on stage"/>
         </div>
     )
 }
