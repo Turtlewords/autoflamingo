b62a420b8f5bb11bb831124a21f1cb6de59b957e
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-11-08 18:20:01 -0600

    import img filepaths

--- src/home.jsx
+++ src/home.jsx
@@ -1,8 +1,9 @@
+import hero from '/assets/af_burlington_lg.jpg'
 
 function Home() {
     return (
         <div className="home fade-in">
-            <img className="hero-img" src="https://github.com/Turtlewords/autoflamingo/blob/main/assets/af_burlington_lg.jpg" alt="autoflamingo on stage"/>
+            <img className="hero-img" src={hero} alt="autoflamingo on stage"/>
         </div>
     )
 }
