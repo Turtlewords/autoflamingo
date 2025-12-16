36dc04ab1fdd3fa919a05d7e9163da24a680be16
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-12-16 14:38:04 -0600

    follow up

--- src/home.jsx
+++ src/home.jsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-=======
-import hero from '/assets/af_burlington_lg.jpg'
->>>>>>> eb1a1cca534d8ffeef5d3f431e32aeefeb44ceff
 
 function Home() {
     return (
         <div className="home fade-in">
-<<<<<<< HEAD
             <img className="hero-img" src="src/assets/af_burlington_lg.jpg" alt="autoflamingo on stage"/>
-=======
-            <img className="hero-img" src={hero} alt="autoflamingo on stage"/>
->>>>>>> eb1a1cca534d8ffeef5d3f431e32aeefeb44ceff
         </div>
     )
 }
